b842afd6a5276e0acf0675e2264ecb6fe5f370a8
Aker <120449218+Aker490@users.noreply.github.com>
2023-02-09 21:59:19 +0700

    Add files via upload

---
+++ bitkub api warn.py
@@ -0,0 +1,29 @@
+import requests
+
+def main(message):
+    url = 'https://notify-api.line.me/api/notify'
+    token = '3fZ9USSjH3l4Tp1s1w66jCAhxINsr2vUiVE6cxCQs2Q'
+    header = {'content-type': 'application/x-www-form-urlencoded',
+              'Authorization': 'Bearer ' + token}
+     
+    response = requests.post(url, headers=header, data={'message': message})
+
+url = 'https://api.bitkub.com/api/market/ticker'
+price = 0
+
+while True:
+    req= requests.get(url)
+    data = req.json()
+    last = data['THB_BTC']['last']
+    high = data['THB_BTC']['high24hr']
+    low = data['THB_BTC']['low24hr']
+
+    if price != last:
+        msg = f"Current Bitcoin price:\n" \
+                    f"ราคา: {last} THB\n" \
+                    f"------------------------\n" \
+                    f"24 H สูงสุด: {high} THB\n" \
+                    f"24 H ต่ำสุด: {low} THB\n" \
+                    f"------------------------\n"
+        main({msg})
+        price = last
